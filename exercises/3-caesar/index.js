4213e73ea7e0f5ba9346d269aa4254d944344b34
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-07-11 15:24:59 +0200

    exercise 3 almost done

--- exercises/3-caesar/index.js
+++ exercises/3-caesar/index.js
@@ -1,8 +1,59 @@
 /**
- *
+ * function encryptes input string according to a Caesar cipher,
+ * moving every character for `num` positions along the alphabet
+ * 
+ * @param {string} inputString;
+ * @param {number} num;
+ * 
+ * @returns {string} 
+ * 
+ * @example 
+ * const res = caesar("Hello",5);
+ * console.log(res) // 'Mjqqt' * 
+ * 
  */
-const caesar = () => {
+const caesar = (inputString, num) => {
+    let res = '';
+
+    num = num % 26;
+    const alphabet = alp();
+
+    for (let i = 0; i < inputString.length; i++) {
+        let index = alphabet.findIndex(e => e === inputString[i].toLowerCase());
+        let char = inputString[i].toLowerCase();
+
+        if (char.charCodeAt() >= 32 && char.charCodeAt() <= 64) {
+            res += char;
+            continue;
+        } else {
+            newIndex = index + num;
+
+            if (newIndex > 25) newIndex = newIndex - 26;
+            else if (newIndex < 0) newIndex = 26 + newIndex;
+
+            res += (inputString[i] === char.toUpperCase()) ? alphabet[newIndex].toUpperCase() : alphabet[newIndex];
+        }
+    }
+
+    console.log(res);
+    return res;
 
 };
+
+function alp() {
+    const start = 'a'.charCodeAt(0);
+    const end = 'z'.charCodeAt(0);
+
+    const alp = [];
+
+    for (let i = start; i <= end; i++) {
+        alp.push(String.fromCharCode(i));
+    }
+    return alp;
+}
+
+alp();
+
+caesar('Mjqqt, Btwqi!', -5);
 
 module.exports = caesar;
--- exercises/3-find-the-oldest/index.js
+++ exercises/3-find-the-oldest/index.js
@@ -1,8 +1,68 @@
 /**
- *
- */
-let findTheOldest = function () {
+ * function finds the name of the oldest person in a given array of objects: {name, yearOfBirth, yearOfDeath}
+ *  @param{arrayOfObjects} arrayOfObjects;
+ *  @returns{Object} {{name, yearOfBirth, yearOfDeath}
+ * 
+ *  @example 
+ *  const obj = findTheOldest([{
+        name: 'Carly',
+        yearOfBirth: 1066
+
+    },
+    {
+        name: 'Ray',
+        yearOfBirth: 1962,
+        yearOfDeath: 2011
+    },
+    {
+        name: 'Jane',
+        yearOfBirth: 1912,
+        yearOfDeath: 1941
+    },
+]);
+*  console.log(obj.name) // Carly
+* 
+* 
+*/
+let findTheOldest = function(arrayOfObjects) {
+    let theOldest = null;
+
+
+    let maxlive = 0;
+    let maxIndex = 0;
+
+    for (let i = 0; i < arrayOfObjects.length; i++) {
+
+        let live = (arrayOfObjects[i].yearOfDeath !== undefined) ? arrayOfObjects[i].yearOfDeath - arrayOfObjects[i].yearOfBirth :
+            new Date().getFullYear() - arrayOfObjects[i].yearOfBirth;
+        if (live > maxlive) {
+            maxlive = live;
+            maxIndex = i;
+        }
+
+    }
+
+    theOldest = arrayOfObjects[maxIndex];
+
+    return theOldest;
 
 }
+
+findTheOldest([{
+        name: 'Carly',
+        yearOfBirth: 1066
+
+    },
+    {
+        name: 'Ray',
+        yearOfBirth: 1962,
+        yearOfDeath: 2011
+    },
+    {
+        name: 'Jane',
+        yearOfBirth: 1912,
+        yearOfDeath: 1941
+    },
+]);
 
 module.exports = findTheOldest;
